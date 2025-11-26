f82d111c828f17d17146a91c3fcaf8a152a25c67
vktrenga <vktrenga@gmail.com>
2025-11-26 22:25:36 +0530

    #FF-003: Timing added

--- fuel_finder_app/admin.py
+++ fuel_finder_app/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import FuelPrices, FuelTypes, Amenities, Cities, FuelStations
+from .models import FuelPrices, FuelStationTimings, FuelTypes, Amenities, Cities, FuelStations
 admin.site.register(FuelTypes)
 admin.site.register(Amenities)
 admin.site.register(Cities)
 admin.site.register(FuelStations)
 admin.site.register(FuelPrices)
+admin.site.register(FuelStationTimings)
